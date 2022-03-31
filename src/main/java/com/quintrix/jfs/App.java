9f70967c37b6b51cb7c4de9288759be445f0adbc
Brandon Northrop <brannorth0414@yahoo.com>
2022-03-31 14:59:24 -0600

    Add sample code

--- src/main/java/com/quintrix/jfs/App.java
+++ src/main/java/com/quintrix/jfs/App.java
@@ -8,8 +8,46 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello!" );
+    	
+    	long biggerNum = 1000L;
+    	
+    	boolean checkNum = false;
+    	
+    	String str1 = new String("Hello");
+    	
+    	String str2 = new String("Hello");
+    	
+    	/*if(str1 == str2) {
+    		System.out.println("They are equal");
+    	} else {
+    		System.out.println("They are not equal");
+    		}
+    	*/
+    	Integer carsCounter1 = 10;
+    	
+    	Integer carsCounter2 = 10;
+    	
+    	if(carsCounter1 == carsCounter2) {
+    		System.out.println("They are equal");
+    	} else {
+    		System.out.println("They are not equal");
+    	}
+    	
+    	int carsCounter = 0;
+    	
+    	char alphabet = '\u2764';
+    	
+    	
+    	double testDecimalVal = 1.0;
+    	
+    	testDecimalVal = testDecimalVal + 0.2;
+    	
+    	
+    
+    	carsCounter = (int) biggerNum;
+    	
+    	carsCounter++;
         
-        System.out.println( "Good Morning!" );
+        System.out.println("Test Output: " + testDecimalVal);
     }
 }
