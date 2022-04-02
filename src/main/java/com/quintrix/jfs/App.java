093d2c80c637b0cf675e7c78e0e76c98a2ae6721
Brandon Northrop <brannorth0414@yahoo.com>
2022-04-01 19:28:01 -0600

    Add changes from 4/1 class

--- src/main/java/com/quintrix/jfs/App.java
+++ src/main/java/com/quintrix/jfs/App.java
@@ -1,53 +1,132 @@
 package com.quintrix.jfs;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+  public static void main(String[] args) {
+
+    long biggerNum = 1000L;
+
+    boolean checkNum = false;
+
+    Map<String, String> stateMap;
     {
-    	
-    	long biggerNum = 1000L;
-    	
-    	boolean checkNum = false;
-    	
-    	String str1 = new String("Hello");
-    	
-    	String str2 = new String("Hello");
-    	
-    	/*if(str1 == str2) {
-    		System.out.println("They are equal");
-    	} else {
-    		System.out.println("They are not equal");
-    		}
-    	*/
-    	Integer carsCounter1 = 10;
-    	
-    	Integer carsCounter2 = 10;
-    	
-    	if(carsCounter1 == carsCounter2) {
-    		System.out.println("They are equal");
-    	} else {
-    		System.out.println("They are not equal");
-    	}
-    	
-    	int carsCounter = 0;
-    	
-    	char alphabet = '\u2764';
-    	
-    	
-    	double testDecimalVal = 1.0;
-    	
-    	testDecimalVal = testDecimalVal + 0.2;
-    	
-    	
-    
-    	carsCounter = (int) biggerNum;
-    	
-    	carsCounter++;
-        
-        System.out.println("Test Output: " + testDecimalVal);
+      stateMap = new HashMap<>();
+      stateMap.put("CO", "Colorado");
+      stateMap.put("OH", "Ohio");
+      stateMap.put("NJ", "New Jersey");
+      stateMap.put("CA", "California");
     }
+
+    String str1 = new String("Hello");
+
+    String str2 = new String("Hello");
+
+    /*
+     * if(str1 == str2) { System.out.println("They are equal"); } else {
+     * System.out.println("They are not equal"); }
+     */
+    Integer carsCounter1 = 10;
+
+    Integer carsCounter2 = 10;
+
+    Date curDate = new Date();
+
+    // System.out.println("curDate is: " + curDate.getTime());
+
+    // System.out.println(Math.pow(5, 3));
+
+    List<Integer> intSet = new ArrayList<>();
+
+    intSet.add(10);
+
+    intSet.add(12);
+
+    intSet.add(15);
+
+    intSet.add(8);
+
+    intSet.add(10);
+
+    intSet.sort(null);
+
+    for (Integer i : intSet) {
+      // System.out.println(i);
+    }
+
+    List<Dog> dogList = new ArrayList<>();
+
+    Dog dog1 = new Dog(1, "A", 2, "Black");
+
+    dogList.add(dog1);
+
+    Dog dog2 = new Dog(2, "B", 1, "White");
+
+    Dog dog3 = new Dog(1, "C", 4, "Blue");
+
+    Dog dog4 = new Dog(4, "A", 2, "Blue");
+
+    dogList.add(dog2);
+
+    dogList.add(dog3);
+
+    dogList.add(dog4);
+
+    dogList.sort(null);
+
+    for (Dog dog : dogList) {
+      System.out.println(dog);
+    }
+
+    if (stateMap.get("CA") != null) {
+      // System.out.println(stateMap.get("CA"));
+    } else {
+      // System.out.println("Not in the map");
+    }
+
+    for (String key : stateMap.keySet()) {
+      // System.out.println(key);
+    }
+    // System.out.println("Dog is: " + dog.getBreed);
+
+    SpecialDog specialDog = new SpecialDog();
+
+    specialDog.fight();
+
+    // DateFormat dateFormat = new SimpleDateFormat("M/d F");
+    // String strDate = dateFormat.format(curDate);
+    // System.out.println("Converted String: " + strDate);
+
+
+    if (carsCounter1 == carsCounter2) {
+      // System.out.println("They are equal");
+    } else {
+      // System.out.println("They are not equal");
+    }
+
+    int carsCounter = 0;
+
+    char alphabet = '\u2764';
+
+
+    double testDecimalVal = 1.0;
+
+    testDecimalVal = testDecimalVal + 0.2;
+
+
+
+    carsCounter = (int) biggerNum;
+
+    carsCounter++;
+
+    // System.out.println("Test Output: " + testDecimalVal);
+  }
 }
---
+++ src/main/java/com/quintrix/jfs/SpecialDog.java
@@ -0,0 +1,27 @@
+package com.quintrix.jfs;
+
+public class SpecialDog extends Dog {
+  private String origin;
+
+  public SpecialDog() {
+
+  }
+
+  public SpecialDog(String origin) {
+    super();
+    this.origin = origin;
+  }
+
+  public String getOrigin() {
+    return origin;
+  }
+
+  public void setOrigin(String origin) {
+    this.origin = origin;
+  }
+
+  public void fight() {
+
+  }
+}
+
